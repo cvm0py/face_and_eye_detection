ba9b69b6dcc488d6f80a9ba4deb931ef349d47ce
Shivam Tyagi <68807093+cvm0py@users.noreply.github.com>
2020-10-08 11:08:35 +0530

    Initial_commit

---
+++ Object_Dection.py
@@ -0,0 +1,29 @@
+
+import cv2
+face_cascade = cv2.CascadeClassifier(r'C:\Users\Shivam\Downloads\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(r'C:\Users\Shivam\Anaconda3\Lib\site-packages\cv2\data\haarcascade_eye.xml')
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame
+
+# Doing some Face Recognition with the webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    ret, frame = video_capture.read()
+    #print(ret)
+    if ret==True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        canvas = detect(gray, frame)
+        cv2.imshow('Video', canvas)
+        if cv2.waitKey(1) & 0xFF == ord('a'):
+            break
+video_capture.release()
+cv2.destroyAllWindows()
+
